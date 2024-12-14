c827edaf291575239b409cffcec92c38fb8c33c7
Neo-poznan <ilapravednikov67@gmail.com>
2024-12-14 05:45:28 +0300

    updated readme

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 from django.urls import reverse_lazy
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l)&tz0($6ufio-x25s9#=b%fg+v^%g9w0x29jdd46rr0mz6^4y'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,12 +78,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'база данных',
+        'USER': 'имя роли',
+        'PASSWORD': 'пароль',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+}
 }
 
 LOGGING = {
